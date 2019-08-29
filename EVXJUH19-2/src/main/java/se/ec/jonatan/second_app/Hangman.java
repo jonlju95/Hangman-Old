f608cb773c3c9e3b9999fe6a7d79ebe36857d8fc
Jonatan <Jonatan@DESKTOP-7MTBJ85>
2019-08-29 10:32:14 +0200

    Second app

---
+++ EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
@@ -0,0 +1,17 @@
+package se.ec.jonatan.second_app;
+
+import java.util.*;
+
+public class Hangman {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String answer = "word";
+		char[] empty = new char[answer.length()];
+		for(int i=0; i<empty.length; i++) {
+			empty[i] = '_';
+		}
+		for(int i=0; i<8; i++) {
+			
+		}
+	}
+}
