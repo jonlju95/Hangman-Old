a4e0f17da6a09e6cbf212b65bea3911bfde299bd
Jonatan <Jonatan@DESKTOP-86JMF5R>
2019-09-01 12:05:20 +0200

    Second app

--- EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
+++ EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
@@ -5,21 +5,36 @@ import java.util.*;
 public class Hangman {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String answer = "word";
+		String answer = "worwd";
+		int tries = 0;
 		char[] empty = new char[answer.length()];
 		for(int i=0; i<empty.length; i++) {
 			empty[i] = '_';
 		}
-		for(int i=0; i<8; i++) {
-			String guess = in.nextLine();
-			if(guess.length()>1) {
-				wholeWord(guess, answer);
+		while(tries<8) {
+			System.out.println("Do you want to guess a letter or the entire word? "
+								+ "Type 1 for letter and 2 for word.");
+			String reply = in.nextLine();
+			if(reply.equals("1")) {
+				oneLetter('w', answer);
+			}
+			else if(reply.equals("2")) {
+				
 			}
 			else {
-				
+				System.out.println("Wrong entry! Try again.");
 			}
 		}
 	}
+	
+	public static void oneLetter(char guess, String answer) {
+		for(int i=0; i<answer.length(); i++) {
+			if(answer.charAt(i)==guess) {
+				System.out.println("test");
+			}
+		}
+	}
+	
 	public static String wholeWord(String guess, String answer) {
 		String check = guess;
 		if(guess.equalsIgnoreCase(answer)) {
