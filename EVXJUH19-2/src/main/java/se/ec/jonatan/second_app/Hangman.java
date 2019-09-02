ffc85e50a01eadf2b480b60cd678fe9f6934f762
Jonatan <Jonatan@DESKTOP-7MTBJ85>
2019-09-02 12:49:44 +0200

    Second app

--- EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
+++ EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
@@ -3,6 +3,9 @@ package se.ec.jonatan.second_app;
 import java.util.*;
 
 public class Hangman {
+	
+	static StringBuilder build = new StringBuilder();
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String answer = "worwd";
@@ -10,39 +13,49 @@ public class Hangman {
 		char[] empty = new char[answer.length()];
 		for(int i=0; i<empty.length; i++) {
 			empty[i] = '_';
+			build.append(empty[i]);
 		}
 		while(tries<8) {
 			System.out.println("Do you want to guess a letter or the entire word? "
-								+ "Type 1 for letter and 2 for word.");
+								+ "\nType 1 for letter and 2 for word.");
 			String reply = in.nextLine();
+			String guess;
 			if(reply.equals("1")) {
-				oneLetter('w', answer);
+				System.out.println("\nGuess a letter: ");
+				guess = in.nextLine();
+				oneLetter(guess.charAt(0), answer);
+				tries++;
+				System.out.println(build.toString());
 			}
 			else if(reply.equals("2")) {
 				
 			}
 			else {
-				System.out.println("Wrong entry! Try again.");
+				System.out.println("Wrong entry! Try again.\n");
 			}
 		}
+		if(build.toString().equals(answer)) {
+			System.out.println("You win!");
+		}
+		else {
+			System.out.println("Game over");
+		}
+		
 	}
 	
 	public static void oneLetter(char guess, String answer) {
 		for(int i=0; i<answer.length(); i++) {
 			if(answer.charAt(i)==guess) {
-				System.out.println("test");
+				build.setCharAt(i, guess);
 			}
 		}
 	}
 	
-	public static String wholeWord(String guess, String answer) {
-		String check = guess;
+	public static void wholeWord(String guess, String answer) {
 		if(guess.equalsIgnoreCase(answer)) {
-			return "Correct";
 		}
 		else {
 			
 		}
-		return check;
 	}
 }
