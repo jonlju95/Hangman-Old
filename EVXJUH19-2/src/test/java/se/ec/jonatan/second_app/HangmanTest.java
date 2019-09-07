3f31e364fe6d70683b38fde429a898147a96963b
Jonatan <Jonatan@DESKTOP-86JMF5R>
2019-09-07 12:58:20 +0200

    Second app

--- EVXJUH19-2/src/test/java/se/ec/jonatan/second_app/HangmanTest.java
+++ EVXJUH19-2/src/test/java/se/ec/jonatan/second_app/HangmanTest.java
@@ -1,14 +1,32 @@
 package se.ec.jonatan.second_app;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+import junit.framework.Assert;
 
-class HangmanTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+public class HangmanTest {
+	Hangman gameTest;
+	
+	@Before
+	public void beforeEach() {
+		gameTest = new Hangman();
+		gameTest.setAnswer();
+		gameTest.setCharArr();
 	}
-
+	
+	@Test
+	public void oneLetter() {
+		gameTest.oneLetter('a', gameTest.getAnswer());
+	}
+	
+	@Test
+	public void wholeWord() {
+		gameTest.wholeWord("java", gameTest.getAnswer());
+	}
+	
+	@Test
+	public void printArray() {
+		gameTest.printArray(gameTest.getCharArr());
+	}
 }
