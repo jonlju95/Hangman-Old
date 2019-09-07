da654ddb99952708d46f5a43cc2f6ec648fc1744
Jonatan <Jonatan@DESKTOP-86JMF5R>
2019-09-07 11:59:20 +0200

    Second app

---
+++ EVXJUH19-2/src/test/java/se/ec/jonatan/second_app/HangmanTest.java
@@ -0,0 +1,14 @@
+package se.ec.jonatan.second_app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HangmanTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
