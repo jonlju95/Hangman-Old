03676fa2b7cfc55a75b964f9c2663d08fdcda0be
Jonatan <Jonatan@DESKTOP-86JMF5R>
2019-09-07 11:52:35 +0200

    Second app

--- EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
+++ EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/Hangman.java
@@ -4,60 +4,55 @@ import java.util.*;
 
 public class Hangman {
 	
-	private static StringBuilder build = new StringBuilder();
-	private static int tries = 0;
-	private static String[] words = {"java", "programming", "computer", "education"};
-	private static char[] empty;
-	private static Random rng = new Random();
-	private static String answer = words[rng.nextInt(words.length)];
+	private StringBuilder build;
+	private int tries;
+	private String[] words;
+	private char[] empty;
+	private Random rng;
+	private String answer;
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public Hangman() {
+		build = new StringBuilder();
+		tries = 0;
+		rng = new Random();
+		words = new String[] {"java", "programming", "computer", "education"};
+	}
+	
+	public int usedTries() {
+		return tries++;
+	}
+	
+	public void setCharArr() {
 		empty = new char[answer.length()];
 		for(int i=0; i<empty.length; i++) {
 			empty[i] = '_';
 		}
-		while(tries<8) {
-			System.out.println(Arrays.toString(empty)+"\n");
-			System.out.println("Do you want to guess a letter or the entire word? "
-								+ "\nType 1 for letter and 2 for word.");
-			String reply = in.nextLine();
-			String guess;
-			if(reply.equals("1")) {
-				System.out.println("\nGuess a letter: ");
-				guess = in.nextLine();
-				if(build.toString().contains(guess)) {
-					System.out.println("You've already guessed that letter");
-				}
-				else {
-					oneLetter(guess.charAt(0), answer);
-					System.out.println();
-				}
-			}
-			else if(reply.equals("2")) {
-				System.out.println("\nGuess the word: ");
-				guess = in.nextLine();
-				wholeWord(guess, answer);
-				tries++;
-			}
-			else {
-				System.out.println("Wrong entry! Try again.\n");
-			}
-			if(printArray(empty).equals(answer)) {
-				System.out.println("You win! \nThe word is: " + answer);
-				in.close();
-				System.exit(0);
-			}
-		}
-		System.out.println("Game over! \nThe word was " + answer);
 	}
 	
-	public static void oneLetter(char guess, String answer) {
+	public char[] getCharArr() {
+		return empty;
+	}
+	
+	public void setAnswer() {
+		answer = words[rng.nextInt(words.length)];
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+	public void setGuesses(String guess) {
 		build.append(guess);
-		System.out.println("Guesses: " + build.toString());
+	}
+	
+	public String getGuesses() {
+		return build.toString();
+	}
+	
+	public void oneLetter(char guess, String answer) {
 		for(int i=0; i<answer.length(); i++) {
 			if(!answer.contains(Character.toString(guess))) {
-				tries++;
+				System.out.println("Wrong\n");
 				return;
 			}
 		}
@@ -68,7 +63,7 @@ public class Hangman {
 		}
 	}
 	
-	public static void wholeWord(String guess, String answer) {
+	public void wholeWord(String guess, String answer) {
 		if(guess.equalsIgnoreCase(answer)) {
 			for(int i=0; i<answer.length(); i++) {
 				empty[i] = guess.charAt(i);
@@ -76,11 +71,10 @@ public class Hangman {
 		}
 		else {
 			System.out.println("Wrong\n");
-			tries++;
 		}
 	}
 	
-	public static String printArray(char[] arr) {
+	public String printArray(char[] arr) {
 		StringBuilder temp = new StringBuilder();
 		for(int i=0; i<arr.length; i++) {
 			temp.append(arr[i]);
---
+++ EVXJUH19-2/src/main/java/se/ec/jonatan/second_app/HangmanMain.java
@@ -0,0 +1,46 @@
+package se.ec.jonatan.second_app;
+
+import java.util.*;
+
+public class HangmanMain {
+	public static void main(String[] args) {
+		Hangman game = new Hangman();
+		Scanner in = new Scanner(System.in);
+		game.setAnswer();
+		game.setCharArr();
+		while(game.usedTries()<8) {
+			System.out.println(Arrays.toString(game.getCharArr())+"\n");
+			System.out.println("Do you want to guess a letter or the entire word? "
+								+ "\nType 1 for letter and 2 for word.");
+			String reply = in.nextLine();
+			String guess;
+			if(reply.equals("1")) {
+				System.out.print("\nGuess a letter: ");
+				guess = in.nextLine();
+				if(game.getGuesses().contains(guess)) {
+					System.out.println("You've already guessed that letter");
+				}
+				else {
+					game.oneLetter(guess.charAt(0), game.getAnswer());
+				}
+				game.setGuesses(guess);
+				System.out.println("\nGuesses: " + game.getGuesses());
+			}
+			else if(reply.equals("2")) {
+				System.out.print("\nGuess the word: ");
+				guess = in.nextLine();
+				game.wholeWord(guess, game.getAnswer());
+				game.usedTries();
+			}
+			else {
+				System.out.println("Wrong entry! Try again.\n");
+			}
+			if(game.printArray(game.getCharArr()).equalsIgnoreCase(game.getAnswer())) {
+				System.out.println("You win! \nThe word is: " + game.getAnswer());
+				in.close();
+				System.exit(0);
+			}
+		}
+		System.out.println("Game over! \nThe word was " + game.getAnswer());
+	}
+}
